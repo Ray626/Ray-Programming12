f48c7663b1235348bfe85f4e8dcaa3932620845a
Ray626 <rayyu626@gmail.com>
2022-02-01 14:52:59 -0800

    firstProject

--- src/firstProject/Main.java
+++ src/firstProject/Main.java
@@ -3,5 +3,8 @@ package firstProject;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        String num = "num";
+
+        System.out.println(num.substring(0,2));
     }
 }
