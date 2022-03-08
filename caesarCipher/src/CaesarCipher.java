514f44c56834105d039633ef9d7dc38ea442900f
Ray626 <90575672+Ray626@users.noreply.github.com>
2022-03-08 14:22:34 -0800

    Caesar Cipher

---
+++ caesarCipher/src/CaesarCipher.java
@@ -0,0 +1,4 @@
+public class CaesarCipher {
+    private String word;
+    
+}
---
+++ caesarCipher/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    static void encrypt(){
+
+    }
+
+    static void decrypt(){
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
