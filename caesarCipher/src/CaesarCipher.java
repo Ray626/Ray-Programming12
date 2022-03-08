0a36e29bf5ab982ace97dcc4c04743e5d0aff76b
Ray626 <90575672+Ray626@users.noreply.github.com>
2022-03-08 15:09:16 -0800

    Caesar Cipher

--- caesarCipher/src/CaesarCipher.java
+++ caesarCipher/src/CaesarCipher.java
@@ -1,4 +1,46 @@
+import java.time.temporal.TemporalAmount;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CaesarCipher {
     private String word;
-    
+    private int shift;
+    private String[] charList = "abcdefghijklmnopqrstuvwxyz".split("");
+
+    private HashMap<String, String>encryptMap = new HashMap<>();
+    private HashMap<String, String>decryptMap = new HashMap<>();
+    public CaesarCipher(String word, int shift) {
+        this.word = word;
+        this.shift = shift;
+        encrypt();
+        decrypt();
+
+    }
+    private void encrypt(){
+        for (int i = 0; i < 26; i++){
+            int shiftAmount = i + shift;
+            if(shiftAmount >= 26){
+                shiftAmount -= 26;
+            }
+            encryptMap.put(charList[i],charList[shiftAmount]);
+        }
+    }
+    private void decrypt(){
+        for (int i = 25; i >= 0; i--){
+            int shiftAmount = i - shift;
+            if (shiftAmount < 0){
+                shiftAmount += 26;
+            }
+            decryptMap.put(charList[i],charList[shiftAmount]);
+        }
+    }
+
+    public String toString(){
+        return encryptMap.toString();
+    }
+
+
+
+
+
 }
--- caesarCipher/src/Main.java
+++ caesarCipher/src/Main.java
@@ -1,15 +1,6 @@
 public class Main {
 
-    static void encrypt(){
-
-    }
-
-    static void decrypt(){
-
-    }
-
-
     public static void main(String[] args) {
-
+        System.out.println(new CaesarCipher("word",26));
     }
 }
