a58c2473ea58d6bffe50e996654e015a088fcddc
Ray626 <90575672+Ray626@users.noreply.github.com>
2022-03-09 14:58:44 -0800

    Caesar Cipher

--- caesarCipher/src/CaesarCipher.java
+++ caesarCipher/src/CaesarCipher.java
@@ -1,16 +1,13 @@
-import java.time.temporal.TemporalAmount;
 import java.util.HashMap;
-import java.util.Map;
 
 public class CaesarCipher {
-    private String word;
-    private int shift;
-    private String[] charList = "abcdefghijklmnopqrstuvwxyz".split("");
-
-    private HashMap<String, String>encryptMap = new HashMap<>();
-    private HashMap<String, String>decryptMap = new HashMap<>();
+    private final String[] word;
+    private final int shift;
+    private final String[] charList = "abcdefghijklmnopqrstuvwxyz".split("");
+    private final HashMap<String, String>encryptMap = new HashMap<>();
+    private final HashMap<String, String>decryptMap = new HashMap<>();
     public CaesarCipher(String word, int shift) {
-        this.word = word;
+        this.word = word.split("");
         this.shift = shift;
         encrypt();
         decrypt();
@@ -19,7 +16,7 @@ public class CaesarCipher {
     private void encrypt(){
         for (int i = 0; i < 26; i++){
             int shiftAmount = i + shift;
-            if(shiftAmount >= 26){
+            while(shiftAmount >= 26){
                 shiftAmount -= 26;
             }
             encryptMap.put(charList[i],charList[shiftAmount]);
@@ -28,15 +25,31 @@ public class CaesarCipher {
     private void decrypt(){
         for (int i = 25; i >= 0; i--){
             int shiftAmount = i - shift;
-            if (shiftAmount < 0){
+            while (shiftAmount < 0){
                 shiftAmount += 26;
             }
             decryptMap.put(charList[i],charList[shiftAmount]);
         }
     }
+    public String encryptWord(){
+        StringBuilder encryptedWord = new StringBuilder();
+        for (String s : word) {
+            encryptedWord.append(encryptMap.get(s));
+        }
+        return encryptedWord.toString();
+    }
+
+    public String decryptWord(){
+        StringBuilder decryptedWord = new StringBuilder();
+        for (String s : word) {
+            decryptedWord.append(decryptMap.get(s));
+        }
+        return decryptedWord.toString();
+    }
+
 
     public String toString(){
-        return encryptMap.toString();
+        return  "String input: " + String.join("", word) +", Shift: " + shift;
     }
 
 
--- caesarCipher/src/Main.java
+++ caesarCipher/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new CaesarCipher("word",26));
+        System.out.println(new CaesarCipher("apple",28).decryptWord());
+        System.out.println(new CaesarCipher("apple", 28));
     }
 }
