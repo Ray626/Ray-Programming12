9eef09aa6a501b5e024d27c6f113015ac32c84d0
Ray626 <90575672+Ray626@users.noreply.github.com>
2022-03-11 13:59:14 -0800

    Caesar Cipher

--- caesarCipher/src/Main.java
+++ caesarCipher/src/Main.java
@@ -9,11 +9,22 @@ public class Main {
         while((line = br.readLine())!= null){
             words.add(line);
         }
+        String[] sentence = "ifmmp xpsme j bn b spcpu".split(" ");
         for (int i = 0; i < 26; i++){
-            CaesarCipher sentenceOne = new CaesarCipher("jgnnq",i);
-            if (words.contains(sentenceOne.decryptWord())){
-                System.out.println(sentenceOne.decryptWord());
+            int num = 0;
+            StringBuilder finalString = new StringBuilder();
+            for (int i1 = 0; i1 < sentence.length; i1++){
+                CaesarCipher sentenceOne = new CaesarCipher(sentence[i1],i);
+                if (words.contains(sentenceOne.decryptWord())) {
+                    num++;
+                    finalString.append(sentenceOne.decryptWord()).append(" ");
+                }
+                if (num == sentence.length){
+                    System.out.println(finalString);
+                }
             }
+
+
         }
     }
 }
