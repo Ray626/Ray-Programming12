0ddd82d1d0aa91e65f4f83cdc2ae62b6e230f60b
Ray626 <90575672+Ray626@users.noreply.github.com>
2022-04-01 14:58:12 -0700

    Hanoi Tower

--- hanoiTower/src/Main.java
+++ hanoiTower/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
-    static int hanoi(int ring){
+    static void hanoi(int ring){
         if (ring == 1){
-            return 1;
+
         }else{
-            return 2*hanoi(ring -1)+1;
+
         }
 
     }
     public static void main(String[] args) {
-        System.out.println(hanoi(3));
+
     }
 }
