a4ed8f82772f011613b89ccf2ebee9a5e001d0ed
Ray626 <rayyu626@gmail.com>
2022-04-01 12:29:18 -0700

    fibonacci

--- fibonacci/src/Main.java
+++ fibonacci/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     static int fibonacci(int term){
-        return term<=1 ? (term== 1 ?1:0) : 2 * fibonacci(term - 1) - fibonacci(term -3);
+        return term <= 1 ? (term== 1 ? 1 : 0) : 2 * fibonacci(term - 1) - fibonacci(term -3);
     }
     public static void main(String[] args) {
         System.out.println(fibonacci(5));
